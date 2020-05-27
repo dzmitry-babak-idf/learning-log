ce21c288dbeade2f910702e83e5b33197e51c73d
Dzmitry Babak <alexgott1990@gmail.com>
2020-05-27 21:59:32 +0300

    ll2

--- bin/start_gunicorn.sh
+++ bin/start_gunicorn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 source /home/www/code/learning-log/env/bin/activate
-exec gunicorn -c "/home/www/code/learning-log/learning-log/gunicorn_conf.py" config.wsgi
+exec gunicorn -c "/home/www/code/learning-log/learning_log/gunicorn_conf.py" learning_log.wsgi
